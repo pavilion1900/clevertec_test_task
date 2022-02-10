99dd4129fc76844ad091c9e6954f1a151664934a
pavilion1900 <pavilion1900@gmail.com>
2022-02-10 15:45:38 +0300

    add tests

---
+++ src/test/java/ru/clevertec/CheckRunnerTest.java
@@ -0,0 +1,82 @@
+package ru.clevertec;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import ru.clevertec.actions.*;
+import ru.clevertec.input.*;
+import ru.clevertec.output.*;
+import ru.clevertec.store.*;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class CheckRunnerTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void whenExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(List.of("0"));
+        Store itemStore = new MemItemsStore();
+        Store cardStore = new MemCardsStore();
+        List<UserAction> actions = List.of(new ExitProgram());
+        new CheckRunner(out).init(in, itemStore, cardStore, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                "Menu:" + ln
+                        + "0. Exit program" + ln
+        ));
+    }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(List.of("1", "0"));
+        Store itemStore = new MemItemsStore();
+        Store cardStore = new MemCardsStore();
+        List<UserAction> actions = List.of(new ExitProgram());
+        new CheckRunner(out).init(in, itemStore, cardStore, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                "Menu:" + ln
+                        + "0. Exit program" + ln
+                        + "Wrong menu number, try again" + ln
+                        + "Menu:" + ln
+                        + "0. Exit program" + ln
+        ));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenOrderIsEmpty() {
+        Output out = new StubOutput();
+        Input in = new StubInput(List.of("0", "", "1"));
+        Store itemStore = new MemItemsStore();
+        Store cardStore = new MemCardsStore();
+        List<UserAction> actions = List.of(new MakeOrderFixedSettings(out), new ExitProgram());
+        new CheckRunner(out).init(in, itemStore, cardStore, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                "Menu:" + ln
+                        + "0. Make order fixed settings" + ln
+        ));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenEnterNotExistItem() {
+        Output out = new StubOutput();
+        Input in = new StubInput(List.of("0", "3-8 2-5 50-7 card-1234", "1"));
+        Store itemStore = new MemItemsStore();
+        Store cardStore = new MemCardsStore();
+        List<UserAction> actions = List.of(new MakeOrderFixedSettings(out), new ExitProgram());
+        new CheckRunner(out).init(in, itemStore, cardStore, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                "Menu:" + ln
+                        + "0. Make order fixed settings" + ln
+        ));
+    }
+}
---
+++ src/test/java/ru/clevertec/input/ValidateInputTest.java
@@ -0,0 +1,41 @@
+package ru.clevertec.input;
+
+import org.junit.Test;
+import ru.clevertec.output.*;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ValidateInputTest {
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(List.of("one", "1"));
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(List.of("1"));
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void checkOutput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(List.of("one", "1"));
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(out.toString(), is(String.format(
+                "Enter validate data%n"
+        )));
+    }
+}
