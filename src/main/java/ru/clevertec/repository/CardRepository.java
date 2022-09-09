a50af3830a2f2802b4073cbb7a7c4dcfb8e8cb73
Korolenko Sergey <pavilion1900@gmail.com>
2022-09-09 10:43:13 +0300

    delete annotation @Repository

--- src/main/java/ru/clevertec/repository/CardRepository.java
+++ src/main/java/ru/clevertec/repository/CardRepository.java
@@ -1,12 +1,10 @@
 package ru.clevertec.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import ru.clevertec.entity.Card;
 
 import java.util.Optional;
 
-@Repository
 public interface CardRepository extends JpaRepository<Card, Integer> {
 
     Optional<Card> findFirstByNumber(Integer number);
--- src/main/java/ru/clevertec/repository/ItemRepository.java
+++ src/main/java/ru/clevertec/repository/ItemRepository.java
@@ -1,10 +1,8 @@
 package ru.clevertec.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import ru.clevertec.entity.Item;
 
-@Repository
 public interface ItemRepository extends JpaRepository<Item, Integer> {
 
 }
